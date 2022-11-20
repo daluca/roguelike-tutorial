9968108099a20697c0b6992d5ce50b096538dde5
Lucas Slebos <lucas@slebos.nz>
2022-11-20 22:55:23 +1300

    linting with clippy

--- src/map.rs
+++ src/map.rs
@@ -2,7 +2,7 @@ use super::rect::Rect;
 use rltk::{RandomNumberGenerator, Rltk, RGB};
 use std::cmp::{max, min};
 
-#[derive(PartialEq, Clone, Copy)]
+#[derive(PartialEq, Eq, Clone, Copy)]
 pub enum TileType {
     Wall,
     Floor,
@@ -10,34 +10,6 @@ pub enum TileType {
 
 pub fn xy_idx(x: i32, y: i32) -> usize {
     (y as usize * 80) + x as usize
-}
-
-/// Make a map with solid boundaries and 400 randomly placed walls. No guarantees that it wont look awful
-pub fn new_map_test() -> Vec<TileType> {
-    let mut map = vec![TileType::Floor; 80 * 50];
-
-    for x in 0..80 {
-        map[xy_idx(x, 0)] = TileType::Wall;
-        map[xy_idx(x, 49)] = TileType::Wall;
-    }
-
-    for y in 0..50 {
-        map[xy_idx(0, y)] = TileType::Wall;
-        map[xy_idx(79, y)] = TileType::Wall;
-    }
-
-    let mut rng = rltk::RandomNumberGenerator::new();
-
-    for _i in 0..400 {
-        let x = rng.roll_dice(1, 79);
-        let y = rng.roll_dice(1, 49);
-        let idx = xy_idx(x, y);
-        if idx != xy_idx(40, 25) {
-            map[idx] = TileType::Wall;
-        }
-    }
-
-    map
 }
 
 pub fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
